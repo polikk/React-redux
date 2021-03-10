6ea5b23201de13b40e1cb42c4a0e6ac86eeab8e7
polikk <polikigor@ukr.net>
2021-03-10 19:22:18 +0200

    Updates

---
+++ precache-manifest.b9fcece8f2410e15e775807510e7de48.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d894ebfc2a17015bc6b67f70bc52064",
+    "url": "/React-redux/index.html"
+  },
+  {
+    "revision": "0262b4167567932fb447",
+    "url": "/React-redux/static/css/main.29ad373f.chunk.css"
+  },
+  {
+    "revision": "e54ee7d68a33312223f7",
+    "url": "/React-redux/static/js/2.b1e9bc0a.chunk.js"
+  },
+  {
+    "revision": "3f598473a645111fe9a1b1e43cd3e4b8",
+    "url": "/React-redux/static/js/2.b1e9bc0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0262b4167567932fb447",
+    "url": "/React-redux/static/js/main.5d610378.chunk.js"
+  },
+  {
+    "revision": "fc963f0b7f6aa00123e6",
+    "url": "/React-redux/static/js/runtime-main.434c98bf.js"
+  }
+]);
